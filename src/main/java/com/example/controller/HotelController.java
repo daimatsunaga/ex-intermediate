422cae7ce172a6fb05c5703b6305843c98b149c9
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-09 11:18:00 +0900

    HotelConterollerの作成

---
+++ src/main/java/com/example/controller/HotelController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Hotel;
+import com.example.service.HotelService;
+
+@Controller
+@RequestMapping("/hotel")
+public class HotelController {
+
+	@Autowired
+	private HotelService hotelService;
+	
+	
+	@RequestMapping("to-index")
+	public String index() {
+		return "hotel_index";
+	}
+	
+	@RequestMapping("/search")
+	public String search(Integer price, Model model) {
+		model.addAttribute("price", price);
+		if(price == null || price <= 300000 && price >= 0) {
+			List<Hotel> hotelList = hotelService.search(price);
+			model.addAttribute("hotelList", hotelList);
+			return "hotel_index";
+		}
+			model.addAttribute("message", "0円~300,000円で検索してください");
+			return "hotel_index";
+
+	}
+}
